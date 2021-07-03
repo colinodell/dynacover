9ada605648ccfd4e457bc50050c8b73cf48f073a
Colin O'Dell <colinodell@gmail.com>
2021-07-03 08:59:07 -0400

    Pull credentials from env vars

---
+++ credentials.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'twitter_consumer_key' => $_ENV['TWITTER_APP_CONSUMER_KEY'],
+    'twitter_consumer_secret' => $_ENV['TWITTER_APP_CONSUMER_SECRET'],
+    'twitter_user_token' => $_ENV['TWITTER_USER_ACCESS_TOKEN'],
+    'twitter_token_secret' => $_ENV['TWITTER_USER_ACCESS_TOKEN_SECRET'],
+
+    'github_api_bearer' => $_ENV['GITHUB_API_BEARER_TOKEN'],
+];
